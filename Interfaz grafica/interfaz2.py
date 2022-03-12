4df501f43b1769e87bd32b95888df3881acf6c18
Lisandro <lisandroasuvercase@gmail.com>
2022-03-12 17:52:30 -0300

    Interfaz gráfica

--- Interfaz grafica/interfaz2.py
+++ Interfaz grafica/interfaz2.py
@@ -11,7 +11,7 @@ root.iconbitmap("podio.ico")
 miFrame = Frame(root) # le estoy indicando que nuestro FRAME va a estar adentro de nuestro root (la raiz)
 
 # miFrame.pack(side=LEFT, anchor="n") #le digo donde quiero que esté mi frame. La "n" significa norte, puedo elegir "s" que es SUR, o las demas.
-miFrame.pack(fill = "x") # lo que hace la "x" es que el frame siga a lo largo a la raiz pero no en lo alto, la "y" hace lo contrario, el expand debería seguirlo en ambos
+miFrame.pack(fill = "both", expand=1) # lo que hace la "x" es que el frame siga a lo largo a la raiz pero no en lo alto, la "y" hace lo contrario, el expand debería seguirlo en ambos
 
 miFrame.config(width=400, height=300)
 miFrame.config(cursor="pirate") #cambio de cursor, también tenemos la opción "boat"
