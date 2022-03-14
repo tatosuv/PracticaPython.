337eb2a597e6aeff4779099ebe2eb88d4df371d3
Lisandro <lisandroasuvercase@gmail.com>
2022-03-14 16:06:13 -0300

    Label

--- Interfaz grafica/label.py
+++ Interfaz grafica/label.py
@@ -6,15 +6,16 @@ root.title("Bienvenidos")
 root.config(width=400, height=300)
 #Para crear una label lo pimero que tenemos que hacer es crear una variable que se llame "label" y la igualo con Label (que pertenece a la librería de tkinter) y establecemos dónde se va a ubicar esa label
 
-label = Label(root, text="Hola mundo!") #establecemos que nuestra label se va a situar en nuestra raiz que se llama root y su texto
+label = Label(root, text="Aguante") #establecemos que nuestra label se va a situar en nuestra raiz que se llama root y su texto
 #si lo ejecutamos no nos aparecerá nada, pues tenemos que empaquetarlo
 
-label.pack()
+label.place(x=100, y=50) #le indicamos con coordenada dónde queremos nuestro Label
+label.config(bg="blue", fg="black", font=("Curier", 20))
 
 
-label = Label(root, text="Bienvenidos")
-label.pack()
-
+label = Label(root, text="Boca!")
+label.place(x=200, y=60)
+label.config(bg="yellow", fg="black", font=("Curier", 20))
 
 
 root.mainloop()
