673bee99adc2db21e339fa8a0a3f02c3e225a295
Lisandro <lisandroasuvercase@gmail.com>
2022-03-14 16:45:21 -0300

    Entry

--- Interfaz grafica/entry.py
+++ Interfaz grafica/entry.py
@@ -6,14 +6,19 @@ frame = Frame(root, width=500, height=400)
 frame.pack()
 
 entrada = Entry(frame) #Nuestro entry va a estar en nuestro frame, nos permite escribir
-entrada.pack()
+entrada.grid(row=0, column=1) # lo posicionamos mediante GRID
+entrada.config(justify="center", state="normal")
+
+entrada2 = Entry(frame)
+entrada2.grid(row=1, column=1)
+entrada2.config(justify="center", show="*")  #le digo que me muestre el texto en formato contraseña
 
 
-label = Label(frame, text="Nombre")
-label.pack()
+label1 = Label(frame, text="Nombre:")
+label1.grid(row=0, column=0)
 
-
-
+label2 = Label(frame, text="Password:")
+label2.grid(row=1, column=0)
 
 
 root.mainloop()
