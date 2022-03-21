33882ebb9e86294ac5d5b5f124b16a2577d2d873
Lisandro <lisandroasuvercase@gmail.com>
2022-03-21 15:36:15 -0300

    Cambiar color de la ventana

--- Tkinter/AskColor.py
+++ Tkinter/AskColor.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter.colorchooser import askcolor
 from turtle import title
 
-#Como cambiar el color de la ventana en el momento
+#Como cambiar el color de la ventana en el momento, en tiempo real
 
 root = tk.Tk()
 root.title("Tkinter Color")
@@ -12,15 +12,7 @@ def cambiarColor():
     colores = askcolor(title="tkinter colores")
     root.configure(bg=colores[1])
 
-tk.Button(
-    root,
-    text="Cambiar color",
-    command=cambiarColor).pack(expand=True)
-
-
-
-
-
+tk.Button(root, text="Cambiar color", command=cambiarColor).pack(expand=True)
 
 
 root.mainloop()
