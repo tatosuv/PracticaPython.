24dec100407292e83f51f429eed9721ee63641fc
Lisandro <lisandroasuvercase@gmail.com>
2022-03-31 16:54:09 -0300

    Calculadora

--- Modulo/calculadora.py
+++ Modulo/calculadora.py
@@ -1,11 +1,13 @@
 def sumar(op1,op2):
+    """Esta función lo que realiza es una suma"""
     print("El resultado de tu suma es: ", op1+op2)
 
-
 def restar(op1,op2):
+    """Esta función lo que realiza es una resta"""
     print("El resultado de tu resta es: ", op1-op2)
 
 def dividir(op1,op2):
+    """Esta función lo que realiza es una división"""
     print("El resultado de tu división es: ", op1/op2)
 
     
