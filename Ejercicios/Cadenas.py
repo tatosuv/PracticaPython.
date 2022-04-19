7e8f49a295ea973323e6ca195ce80226cac0d8b6
Lisandro <lisandroasuvercase@gmail.com>
2022-04-18 22:39:57 -0300

    Ejercicios de cadenas

--- Ejercicios/Cadenas.py
+++ Ejercicios/Cadenas.py
@@ -30,7 +30,15 @@ print(nombre.upper() + " tiene " + str(len(nombre)) + " letras.")
 '''
 #Ejercicio4
 #Los teléfonos de una empresa tienen el siguiente formato "prefijo-número-extension" donde el prefijo es el código del país +34, y la extensión tiene dos dígitos (por ejemplo +34-913724710-56). Escribir un programa que pregunte por un número de teléfono con este formato y muestre por pantalla el número de teléfono sin el prefijo y la extensión.
-
+'''
 tel = input("Introduce un número de teléfono con el formato +xx-xxxxxxxxx-xx: ")
 print('El número de teléfono es ', tel[4:-3])
+'''
+# Ejercicio 5
+# Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida.
+
+frase = input("Escriba una frase: ")
+
+print(frase[::-1])
+
 
