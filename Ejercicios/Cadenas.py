1b7ffde468828d197ab882a48c71891053d84941
Lisandro <lisandroasuvercase@gmail.com>
2022-04-18 22:33:39 -0300

    Ejercicios de cadenas

--- Ejercicios/Cadenas.py
+++ Ejercicios/Cadenas.py
@@ -18,7 +18,7 @@ print(nombre.capitalize()) # puede ir title() tambien
 '''
 #Ejercicio3
 #Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla <NOMBRE> tiene <n> letras, donde <NOMBRE> es el nombre de usuario en mayúsculas y <n> es el número de letras que tienen el nombre.
-
+'''
 nombre = input("Escriba su nombre: ")
 
 print(nombre.upper() + " tiene " + str(nombre.__len__()) + " letras.")
@@ -27,9 +27,10 @@ print(nombre.upper() + " tiene " + str(nombre.__len__()) + " letras.")
 nombre = input("Escriba su nombre: ")
 
 print(nombre.upper() + " tiene " + str(len(nombre)) + " letras.")
-
+'''
 #Ejercicio4
 #Los teléfonos de una empresa tienen el siguiente formato "prefijo-número-extension" donde el prefijo es el código del país +34, y la extensión tiene dos dígitos (por ejemplo +34-913724710-56). Escribir un programa que pregunte por un número de teléfono con este formato y muestre por pantalla el número de teléfono sin el prefijo y la extensión.
 
-
+tel = input("Introduce un número de teléfono con el formato +xx-xxxxxxxxx-xx: ")
+print('El número de teléfono es ', tel[4:-3])
 
