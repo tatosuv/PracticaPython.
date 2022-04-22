2f4bd8c57352816dd7bdfd76e456243e95d08abf
Lisandro <lisandroasuvercase@gmail.com>
2022-04-22 17:40:39 -0300

    Ejercicios de Cadenas

--- Ejercicios/Cadenas.py
+++ Ejercicios/Cadenas.py
@@ -36,9 +36,26 @@ print('El número de teléfono es ', tel[4:-3])
 '''
 # Ejercicio 5
 # Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida.
-
+'''
 frase = input("Escriba una frase: ")
 
 print(frase[::-1])
+'''
+# Ejercicio 6
+# Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal, y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula.
+'''
+frase = input("Introduzca una frase: ")
+vocal = input("Y ahora introduzca una vocal en minúscula: ")
 
 
+print("Tu frase es: "+ frase + ", y tu vocal es " + vocal.upper())
+print(frase.replace(vocal,vocal.upper()))
+'''
+# Ejercicio 7
+# Escribir un programa que pregunte el correo electrónico del usuario en la consola y muestre por pantalla otro correo electrónico con el mismo nombre (la parte delante de la arroba @) pero con dominio ceu.es.
+
+correo = input("Escriba su mail: ")
+# NuevoCorreo = "@ceu.es" # esto no va
+
+print(correo[:correo.find("@")] + "@ceu.es")
+# print(correo.replace(correo.find("@"),NuevoCorreo)) # esto no va
