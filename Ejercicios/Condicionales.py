89151f11bd5720963eb3c985030164dfc8149a76
Lisandro <lisandroasuvercase@gmail.com>
2022-04-23 01:18:06 -0300

    Ejercicios de Condicionales

---
+++ Ejercicios/Condicionales.py
@@ -0,0 +1,9 @@
+# Ejercicio 1
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 18:
+    print("Bien, sos mayor de edad!! Bravo!!")
+else:
+    print("No capo, estás verde todavía, te falta madurar.")
