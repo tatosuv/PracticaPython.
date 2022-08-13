601da0e608a3ead4584bda09537fb72a187dd656
Lisandro <lisandroasuvercase@gmail.com>
2022-08-13 18:17:32 -0300

    Practica con condicionales

--- Ejercicios/Condicionales.py
+++ Ejercicios/Condicionales.py
@@ -61,7 +61,7 @@ else:
 
 # Ejercicio 6
 #Los alumnos de un curso se han dividido en dos grupos A y B de acuerdo al sexo y el nombre. El grupo A esta formado por las mujeres con un nombre anterior a la M y los hombres con un nombre posterior a la N y el grupo B por el resto. Escribir un programa que pregunte al usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
-
+'''
 nombre = input("Ingrese su nombre: ")
 sexo = input("Ingrese su sexo (F/M): ")
 
@@ -77,3 +77,29 @@ else:
         grupo = "B"
 
 print("Tu grupo es " + grupo)
+
+'''
+# Ejercicio 7
+'''# Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes:
+
+Renta	Tipo impositivo
+Menos de 10000€	5%
+Entre 10000€ y 20000€	15%
+Entre 20000€ y 35000€	20%
+Entre 35000€ y 60000€	30%
+Más de 60000€	45%
+Escribir un programa que pregunte al usuario su renta anual y muestre por pantalla el tipo impositivo que le corresponde.
+'''
+
+renta = float(input('Cual es su renta anual?: '))
+
+if renta < 10000:
+    print("Tu tipo impositivo es del 5%")
+elif renta > 10000 and renta <= 20000:
+    print("Tu tipo impositivo es del 15%")
+elif renta > 20000 and renta <= 35000:
+    print("Tu tipo impositivo es del 20%")
+elif renta > 35000 and renta <= 60000:
+    print("Tu tipo impositivo es del 30%")
+elif renta > 60000:
+    print("Tu tipo impositivo es del 45%")
